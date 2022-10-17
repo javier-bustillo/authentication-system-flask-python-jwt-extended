43e9e7b384d85d01fdd7f7af8575580eb71ffbd5
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-17 17:56:19 +0000

    practice finished

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person
 #from models import Person
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
